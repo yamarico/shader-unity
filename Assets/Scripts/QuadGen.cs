9d5c505ab467b041e96126051a70fb0a73bf63cb
yamarico <81962317+yamarico@users.noreply.github.com>
2023-01-01 21:39:32 +0900

    toon shader

--- Assets/Scripts/QuadGen.cs
+++ Assets/Scripts/QuadGen.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class QuadGen : MonoBehaviour
 {
+    public Material mat;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Mesh mesh = new Mesh();
+        mesh.vertices = new Vector3[]{
+            new Vector3 (-1.2f, -1.2f,0),
+            new Vector3 (-1.2f, 1.2f,0),
+            new Vector3 (1.2f, -1.2f,0),
+            new Vector3 (1.2f, 1.2f,0),
+        };
+
+        mesh.uv = new Vector2[]{
+            new Vector3(0, 0),
+            new Vector3(0, 1),
+            new Vector3(1, 0),
+            new Vector3(1, 1),
+        };
+
+        mesh.triangles = new int[]{
+            0, 1, 2, 
+            1, 3, 2,
+        };
+
+        GetComponent<MeshFilter>().sharedMesh = mesh;
+        GetComponent<MeshRenderer>().material = mat;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
